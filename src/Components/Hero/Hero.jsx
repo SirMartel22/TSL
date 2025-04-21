440c200c867be4e57eb99a827d8f7b33287a2108
Devtimmy <oyinloyematthew6@gmail.com>
2025-04-21 20:47:39 +0100

    commented unwanted code

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React,{ useState, useEffect, useRef } from 'react'
-import video1 from '../../assets/video1.mp4'
-import video2 from '../../assets/video2.mp4'
+// import video1 from '../../assets/video1.mp4'
+// import video2 from '../../assets/video2.mp4'
 import bgImg from '../../assets/bg-img.png'
 import { Gift } from "lucide-react"
 
